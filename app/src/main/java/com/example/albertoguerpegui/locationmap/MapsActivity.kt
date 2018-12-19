2b606ab29d7822d8671a6421f7e4e8283d5569e5
Alberto.Gurpegui <alberto.gurpeguiramon@gmail.com>
2018-12-19 13:32:32 +0100

    Initial commit

---
+++ app/src/main/java/com/example/albertoguerpegui/locationmap/MapsActivity.kt
@@ -0,0 +1,146 @@
+package com.example.albertoguerpegui.locationmap
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
+import com.google.android.gms.common.GooglePlayServicesRepairableException
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.places.ui.PlacePicker
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
+
+    companion object {
+        private const val PLACE_PICKER_REQUEST = 1
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 2
+    }
+
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var mLocationPermissionGranted = false
+    private var sydney = LatLng(-34.0, 151.0)
+    private var mLastKnownLocation: Location? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+
+        fab.setOnClickListener {
+            loadPlacePicker()
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun loadPlacePicker() {
+        val builder = PlacePicker.IntentBuilder()
+        try {
+            startActivityForResult(builder.build(this@MapsActivity), PLACE_PICKER_REQUEST)
+        } catch (e: GooglePlayServicesRepairableException) {
+            e.printStackTrace()
+        } catch (e: GooglePlayServicesNotAvailableException) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PLACE_PICKER_REQUEST){
+            var place = PlacePicker.getPlace(this,data)
+            map.addMarker(MarkerOptions().position(place.latLng).title(place.name.toString()))
+            map.moveCamera(CameraUpdateFactory.newLatLng(place.latLng))
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.addMarker(MarkerOptions().position(sydney).title("Sydney"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map.uiSettings.isZoomControlsEnabled = true
+        updateLocationUI()
+        getDeviceLocation()
+    }
+
+    private fun updateLocationUI() {
+        try {
+            if (mLocationPermissionGranted) {
+                map.isMyLocationEnabled = true
+                map.uiSettings.isMyLocationButtonEnabled = true
+                fusedLocationClient.lastLocation
+            }
+            else {
+                map.isMyLocationEnabled = false
+                map.uiSettings.isMyLocationButtonEnabled = false
+                //sydney = null
+                getLocationPermission()
+            }
+        }
+        catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message)
+        }
+    }
+
+
+    private fun getDeviceLocation() {
+        try {
+            if (mLocationPermissionGranted) {
+                val locationResult = fusedLocationClient.lastLocation
+                locationResult.addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        mLastKnownLocation = it.result
+                        map.moveCamera(
+                            CameraUpdateFactory.newLatLngZoom(
+                                mLastKnownLocation?.let { location ->
+                                    LatLng(location.latitude,
+                                        location.longitude)
+                                } ?: kotlin.run {
+                                    sydney
+                                }, 10f)
+                        )
+                    } else {
+                        Log.d("error", "Current location is null. Using defaults.")
+                        Log.e("exception", "Exception: %s", it.exception)
+                        map.uiSettings.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        }
+        catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message)
+        }
+    }
+
+
+
+    private fun getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this.applicationContext, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true
+            updateLocationUI()
+        }
+        else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        }
+    }
+
+}
